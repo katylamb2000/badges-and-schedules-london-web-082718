9ecc8e57bb43b76fddb532d8810a686891d0b30b
katy@karstaway.com <katy@karstaway.com>
2018-08-25 14:15:56 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker (name)
+"Hello, my name is #{name}."
+
+end
+
+def batch_badge_creator (attendees)
+  badges = []
+  
+  attendees.each {|x| badges.push "Hello, my name is #{x}."}
+  badges
+ end 
+
+def assign_rooms (attendees)
+  rooms = []
+  attendees.each_with_index {|value, index| rooms.push "Hello, #{value}! You'll be assigned to room #{index + 1}!"}
+  rooms
+end
+
+def printer 
+  print batch_badge_creator.each {|x|}
+
+end
